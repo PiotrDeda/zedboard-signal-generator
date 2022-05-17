b26bf0f7d819ca467ea703e82083b2572bd8e141
Piotr Deda <deda@student.agh.edu.pl>
2022-05-17 18:07:24 +0200

    Setup 1

---
+++ SignalGenerator.srcs/sources_1/new/SPI.sv
@@ -0,0 +1,24 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 05/17/2022 06:04:35 PM
+// Design Name: 
+// Module Name: SPI
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module SPI();
+endmodule
