9126cbeb04b6c56404112b00d7ef1001cabae82f
LukBart <lukbartoszek@gmail.com>
2022-05-31 18:30:41 +0200

    all generators

--- DataGeneration/main.py
+++ DataGeneration/main.py
@@ -13,6 +13,25 @@ def generate_exponential():
     x = 5.57 / SAMPLES
     for i in range (SAMPLES):
         file.write(f'{math.floor(math.exp(i * x)):0{2}x}' + '\n')
+    file.close()
+
+def generate_saw():
+    file = open("saw.mem", 'w')
+    x = 255 / SAMPLES
+    for i in range (SAMPLES):
+        file.write(f'{math.floor(i * x):0{2}x}' + '\n')
+    file.close()
+def generate_triangles():
+    file = open("tri.mem", 'w')
+    x = 255 / SAMPLES * 2
+    for i in range(math.ceil(SAMPLES/2)):
+        file.write(f'{math.floor(i * x):0{2}x}' + '\n')
+    for i in range(math.ceil(SAMPLES/2)):
+        file.write(f'{math.floor((x*SAMPLES/2) - (i * x)):0{2}x}' + '\n')
+    file.close()
+
 if __name__ == '__main__':
     generate_sin()
     generate_exponential()
+    generate_saw()
+    generate_triangles()
