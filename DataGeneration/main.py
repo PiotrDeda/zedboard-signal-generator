66c345a8af8ee172d870e0d8976bd99642593981
LukBart <lukbartoszek@gmail.com>
2022-05-31 17:57:41 +0200

    exponential generator

--- DataGeneration/main.py
+++ DataGeneration/main.py
@@ -3,10 +3,16 @@ import math
 SAMPLES = 200
 
 def generate_sin():
-    file = open ("sin.dat", 'w')
+    file = open ("sin.mem", 'w')
     x = 2 * math.pi / SAMPLES
     for i in range (SAMPLES):
         file.write(f'{math.floor(math.sin(i*x) * 124) + 124:0{2}x}' + '\n')
-
+    file.close()
+def generate_exponential():
+    file = open ("exp.mem", 'w')
+    x = 5.57 / SAMPLES
+    for i in range (SAMPLES):
+        file.write(f'{math.floor(math.exp(i * x)):0{2}x}' + '\n')
 if __name__ == '__main__':
     generate_sin()
+    generate_exponential()
